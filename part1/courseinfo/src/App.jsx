a4e98a0d8f2ab4376426a4c457ef4f7cb72f494d
kran-ji <jkran2252@gmail.com>
2026-05-15 16:55:20 +0800

    Updated part1 exercise 1.1 and 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,31 +9,45 @@ function App() {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course = {course} />
+      <Content part1 = {part1} exercises1 = {exercises1} part2 = {part2} exercises2 = {exercises2} part3 = {part3} exercises3 = {exercises3} />
+      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
     </div>
   )
 }
 
-function Header() {
-
+function Header(props) {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
 }
 
-function Content() {
-
+function Content(props) {
+  console.log(props)
+  return (
+    <div>
+      <Part part = {props.part1} exercise = {props.exercises1} />
+      <Part part = {props.part2} exercise = {props.exercises2} />
+      <Part part = {props.part3} exercise = {props.exercises3} />
+    </div>
+  )
 }
 
-function Total() {
+function Part(props) {
+  return (
+    <p>{props.part} {props.exercise}</p>
+  )
+}
 
+function Total(props) {
+  return (
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
 }
 
 export default App
